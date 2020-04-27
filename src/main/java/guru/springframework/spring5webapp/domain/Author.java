eba154aadc746c8130c3ed197450b704f53e181d
michaelnunn3 <michaelnunn@ufl.edu>
2020-04-27 17:32:37 -0500

    Working JPA database

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring5webapp.domain;
 
+import java.util.HashSet;
 import java.util.Set;
 import javax.persistence.*;
 
@@ -14,16 +15,15 @@ public class Author {
 	private String lastname;
 	
 	@ManyToMany(mappedBy = "authors")
-	private Set<Book> books;
+	private Set<Book> books = new HashSet<>();
 	
 	public Author() {
 		
 	}
 	
-	public Author(String firstname, String lastname, Set<Book> books) {
+	public Author(String firstname, String lastname) {
 		this.firstname = firstname;
 		this.lastname = lastname;
-		this.books = books;
 	}
 	
 	public long getId() {
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring5webapp.domain;
 
+import java.util.HashSet;
 import java.util.Set;
 import javax.persistence.*;
 
@@ -13,19 +14,21 @@ public class Book {
 	private String title;
 	private String isbn;
 	
+	@ManyToOne
+	private Publisher publisher;
+	
 	@ManyToMany
 	@JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), 
 		inverseJoinColumns = @JoinColumn(name = "author_id"))
-	private Set<Author> authors;
+	private Set<Author> authors = new HashSet<>();
 	
 	public Book() {
 		
 	}
 	
-	public Book(String title, String isbn, Set<Author> authors) {
+	public Book(String title, String isbn) {
 		this.title = title;
 		this.isbn = isbn;
-		this.authors = authors;
 	}
 
 	public long getId() {
@@ -58,6 +61,14 @@ public class Book {
 
 	public void setAuthors(Set<Author> authors) {
 		this.authors = authors;
+	}
+
+	public Publisher getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(Publisher publisher) {
+		this.publisher = publisher;
 	}
 
 	@Override
